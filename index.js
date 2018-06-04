19b83b43fa14588399b431a2b88c3264fc0e99cc
Carol Enache <enache.carol@gmail.com>
2018-06-04 23:46:55 +0100

    refactor the validation

--- index.js
+++ index.js
@@ -19,16 +19,12 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result  = Joi.validate(req.body, schema);
-
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body)
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
     }
-
+    
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -48,18 +44,23 @@ app.put('/api/course/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('The course with the given ID was not found');
 
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body)
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
     }
 
     course.name = req.body.name;
     res.send(course);
 
 });
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema)
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listen on port ${port}...`));
